d2b33ca1c11080f51d5ccc00d3b0827f99186b18
murali4598 <77327329+murali4598@users.noreply.github.com>
2021-05-26 17:45:28 +0530

    testing 2

---
+++ IUserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Entities;
+using WebApi.Models;
+
+namespace WebApi.Interfaces
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        User Create(User user, string password);
+        void Update(User user, string password );
+        void UpdatePassword(User user, ChangePassword changePassword);
+        public void UpdatePasswordOtp(User user, string changePassword);
+        void Delete(int id);
+    }
+
+}
