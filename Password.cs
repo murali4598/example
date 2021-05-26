315c434b257fb89d407416bfd8e90e5b98e71c02
murali4598 <77327329+murali4598@users.noreply.github.com>
2021-05-26 18:29:31 +0530

    ttesting 3

---
+++ Password.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApi.Entities;
+
+namespace WebApi.Models
+{
+    public class Password
+    {
+        [Key]
+        public int PId { get; set; }
+        public string OTP { get; set; }
+        public int Id { get; set; }
+
+        [ForeignKey("Id")]
+        public User Users { get; set; }
+
+        public int IsActive { get; set; }
+        public DateTime? OTPExpires { get; set; }
+
+    }
+}
