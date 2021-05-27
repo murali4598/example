2b8e53c82dfbaaf2c768dbca4f18258475f72fe3
murali4598 <77327329+murali4598@users.noreply.github.com>
2021-05-27 11:39:57 +0530

    Add files via upload

---
+++ ChangePassword.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class ChangePassword
+    {
+        [Required]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
